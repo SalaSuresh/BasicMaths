58bc8dd24ec82b4ef937c52bbd50c1af95f66586
Suresh Sala <Suresh Sala>
2016-04-17 19:44:13 +0530

    Beginner mode fragment in progress

---
+++ app/src/main/java/com/suresh/basicmaths/ModeBeginner.java
@@ -0,0 +1,20 @@
+package com.suresh.basicmaths;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sures on 4/17/2016.
+ */
+public class ModeBeginner extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+        return view;
+    }
+}
